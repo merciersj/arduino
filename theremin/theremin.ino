18b8802b0675bda33fc9f59e9a2ff2b0c6af8c1c
Sarah <mercier.sj@gmail.com>
2017-07-02 18:27:12 -0400

    completed theremin project

--- theremin/theremin.ino
+++ theremin/theremin.ino
@@ -3,13 +3,36 @@ int sensorLow = 1023;
 int sensorHigh = 0;
 
 const int ledPin = 13;
+const int photoPin = A0;
+const int soundPin = 8;
+const int soundLength = 20;
+const int calibrationTime = 5000;
+
+const int arduoLow = 50;
+const int arduoHigh = 4000;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  while( millis() < calibrationTime ) {
+    sensorVal = analogRead(photoPin);
+    if( sensorVal > sensorHigh ) {
+      sensorHigh = sensorVal;
+    }
+    if( sensorVal < sensorLow ) {
+      sensorLow = sensorVal;
+    }
+  }
 
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorVal = analogRead(photoPin);
 
+  int pitch = map( sensorVal, sensorLow, sensorHigh, arduoLow, arduoHigh );
+  tone(soundPin, pitch, soundLength);
+
+  delay(10);
 }
